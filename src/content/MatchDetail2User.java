ae18221843ca65e0345caddc974e37acd62741bd
K0p3Hb <vlkornilo@gmail.com>
2016-11-21 21:53:48 +0500

    2 зона
    доделал отображение слотов кигроков, но пока не разместил их напротив
    друг друга.

--- src/content/MatchDetail2User.java
+++ src/content/MatchDetail2User.java
@@ -34,7 +34,7 @@ public class MatchDetail2User extends HttpServlet {
     try {
       long id = Long.valueOf(request.getParameter("id"));
       // long id = 1762832017L;
-      String mathOverView = new String(), slots = new String(), perks = new String();
+      String mathOverView = new String(), slotsRadiant = new String(), slotsDire = new String(), perks = new String();
       
       // создаём пустой коннект к базе
       Connection connect = null;
@@ -59,31 +59,50 @@ public class MatchDetail2User extends HttpServlet {
               + rs.getLong(1) + "</td><td>" + rs.getInt(2) / 60 + ":" + rs.getInt(2) % 60 + "</td><td>" + rs.getInt(3)
               + "</td><td>" + "</td><td></table>";
         }
-        while (rs1.next())
-          if (rs1.getInt(4) < 5)
-            slots += ("<div ><table width = 50%, border = 2px><tr><th width = 30%>1</th><th>2</th><th>3</th><th>4</th></tr>"
-                + "<tr align = \"center\"><td>" + rs1.getInt(2) + "</td><td>" + rs1.getLong(3)
+        int num;
+        while (rs1.next()) {
+          num = rs1.getInt(4);
+          if (num < 5)
+            slotsRadiant += ("<div ><table width = 50%, border = 2px>" + "<tr align = \"center\"><td width = 30%>"
+                + rs1.getInt(2) + "</td><td width = 30%>" + rs1.getLong(3)
                 + " </td><td><table><tr align = \"center\"><td>" + rs1.getLong(5) + "</td><td>" + rs1.getLong(6)
-                + "</td><td>" + rs1.getLong(7) + "</td></tr></table></td><td>" + (rs1.getLong(14) / (rs.getInt(2) / 60))
-                + "</td></tr>" + "<tr align = \"center\"><td>" + "<table><tr align = \"center\"><td>" + rs1.getLong(8)
-                + "</td><td>" + rs1.getLong(9) + "</td><td>" + rs1.getLong(10) + "</td></tr>"
-                + "<tr align = \"center\"><td>" + rs1.getLong(11) + "</td><td>" + rs1.getLong(12) + "</td><td>"
-                + rs1.getLong(13) + "</td></tr></table>" + "</td><td><table><tr><td>" + rs1.getLong(19)
-                + "</td></tr><tr><td>" + rs1.getLong(20) + "</td></tr></table></td><td>" + rs1.getInt(21)
-                + "</td><td><table><tr><td>" + (rs1.getLong(16) / (rs.getInt(2) / 60))
+                + "</td><td>" + rs1.getLong(7) + "</td></tr></table></td><td width = 10%>"
+                + (rs1.getLong(14) / (rs.getInt(2) / 60)) + "</td></tr><tr align = \"center\"><td>"
+                + "<table><tr align = \"center\"><td>" + rs1.getLong(8) + "</td><td>" + rs1.getLong(9) + "</td><td>"
+                + rs1.getLong(10) + "</td></tr>" + "<tr align = \"center\"><td>" + rs1.getLong(11) + "</td><td>"
+                + rs1.getLong(12) + "</td><td>" + rs1.getLong(13) + "</td></tr></table>" + "</td><td><table><tr><td>"
+                + rs1.getLong(19) + "</td></tr><tr><td>" + rs1.getLong(20) + "</td></tr></table></td><td>"
+                + rs1.getInt(21) + "</td><td><table><tr><td>" + (rs1.getLong(16) / (rs.getInt(2) / 60))
                 + "</td></tr><tr><td>" + new BigDecimal((rs1.getLong(18) / (rs.getInt(2) / 60f)))
                     .setScale(1, RoundingMode.HALF_UP).floatValue()
                 + "</td></tr></table></td></tr>" + "</table></div>");
-                
+          if (num > 127)
+            slotsDire += ("<div ><table width = 50%, border = 2px>" + "<tr align = \"center\"><td width = 10%>"
+                + (rs1.getLong(14) / (rs.getInt(2) / 60)) + "</td><td width = 30%><table><tr align = \"center\"><td>"
+                + rs1.getLong(5) + "</td><td>" + rs1.getLong(6) + "</td><td>" + rs1.getLong(7)
+                + "</td></tr></table></td><td width = 30%>" + rs1.getLong(3) + " </td><td>" + rs1.getInt(2)
+                + "</td></tr>" + "<tr align = \"center\"><td><table><tr><td>" + (rs1.getLong(16) / (rs.getInt(2) / 60))
+                + "</td></tr><tr><td>"
+                + new BigDecimal((rs1.getLong(18) / (rs.getInt(2) / 60f))).setScale(1, RoundingMode.HALF_UP)
+                    .floatValue()
+                + "</td></tr></table></td><td>" + rs1.getInt(21) + "</td><td><table><tr><td>" + rs1.getLong(19)
+                + "</td></tr><tr><td>" + rs1.getLong(20) + "</td></tr></table></td><td>"
+                + "<table><tr align = \"center\"><td>" + rs1.getLong(8) + "</td><td>" + rs1.getLong(9) + "</td><td>"
+                + rs1.getLong(10) + "</td></tr>" + "<tr align = \"center\"><td>" + rs1.getLong(11) + "</td><td>"
+                + rs1.getLong(12) + "</td><td>" + rs1.getLong(13) + "</td></tr></table>" + "</td>" + "</tr>"
+                + "</table></div>");
+        }
       } catch (Exception e) {
         e.printStackTrace();
       }
       
       PrintWriter out = response.getWriter();
-      out.write("<!DOCTYPE html>\n" + "<html>\n"
-          + "<head><meta charset=\"windows-1251\"><style type = \"text/css\">.layer1, .layer2 {background: #F2EFE6; border: 1px solid #B25538; padding: 10px; margin: 20px;}</style><title>Details for Match</title></head>\n"
-          + "<body bgcolor=\"#fdf5e6\"><div class=\"layer1\">" + mathOverView + "</div>" + "<div class=\"layer2\">"
-          + slots + "</div>" + "</body></html>");
+      out.write("<!DOCTYPE html>\n" + "<html>\n" + "<head><meta charset=\"windows-1251\"><style type = \"text/css\">"
+          + ".layer1, .layer2 {background: #F2EFE6; border: 1px solid #B25538; padding: 10px; margin: 20px;},"
+          + ".radiant{float: left},.dire{float: right}</style><title>Details for Match</title></head>\n"
+          + "<body bgcolor=\"#fdf5e6\"><div class=\"layer1\">" + mathOverView + "</div>"
+          + "<p><div class = \"radiant\">" + slotsRadiant + "</div></p>" + "<div class = \"dire\">" + slotsDire
+          + "</div>" + "</body></html>");
     } catch (Exception e) {
       e.printStackTrace();
     }
